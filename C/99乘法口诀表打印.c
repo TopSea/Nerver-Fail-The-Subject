4c8521caadb0a4d588970be756901dc8ab288342
TopSea <44743008+TopSea@users.noreply.github.com>
2020-07-04 09:06:33 +0800

    Add files via upload

---
+++ C/99乘法口诀表打印.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void mul99() {
+    int i = 1;
+    int j = 1;
+    for (i; i <= 9; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d * %d = %d\t", j, i, i * j);
+        }
+        printf("\n");
+    }
+}
+void main()
+{
+    mul99();
+}
